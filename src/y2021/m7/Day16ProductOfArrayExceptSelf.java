0c19a17c0699322ab4ca14854d3e8ff3a916c427
finezhaooo <635388416@qq.com>
2021-07-16 18:24:52 +0800

    238. 除自身以外数组的乘积

---
+++ src/y2021/m7/Day16ProductOfArrayExceptSelf.java
@@ -0,0 +1,133 @@
+package y2021.m7;
+
+/**
+ * @ClassName : Day16ProductOfArrayExceptSelf
+ * @Description: 238. 除自身以外数组的乘积
+ * 给你一个长度为n的整数数组nums，其中n > 1，返回输出数组output，其中 output[i]等于nums中除nums[i]之外其余各元素的乘积。
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ *
+ *
+ * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+ *
+ * 说明: 请不要使用除法，且在O(n) 时间复杂度内完成此题。
+ *
+ * 进阶：
+ * 你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/product-of-array-except-self
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author zhaooo
+ * @Date 2021/7/16/17:51
+ */
+public class Day16ProductOfArrayExceptSelf {
+    /**
+     * 暴力 一个超时
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        int tmp = 1;
+        for (int i = 0; i < len; i++) {
+            res[i] = tmp;
+            for (int j = 0; j < i; j++) {
+                res[j] *= nums[i];
+            }
+            tmp *= nums[i];
+        }
+        return res;
+    }
+
+    /**
+     * 左右数组
+     * 左数租等于该点左侧所有元素乘积
+     * 右数租等于该点右侧所有元素乘积
+     * 该点结果为左右数组相乘
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf2(int[] nums) {
+        int length = nums.length;
+
+        // L 和 R 分别表示左右两侧的乘积列表
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        int[] answer = new int[length];
+
+        // L[i] 为索引 i 左侧所有元素的乘积
+        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] 为索引 i 右侧所有元素的乘积
+        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+
+    /**
+     * 先把输出数组当作 L 数组来计算，然后再动态构造 R 数组得到结果
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf3(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
+    }
+
+    /**
+     * 计算矩阵的上下三角
+     * 链接：https://leetcode-cn.com/problems/product-of-array-except-self/solution/product-of-array-except-self-shang-san-jiao-xia-sa/
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf4(int[] nums) {
+        int[] res = new int[nums.length];
+        int p = 1, q = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = p;
+            p *= nums[i];
+        }
+        for (int i = nums.length - 1; i > 0 ; i--) {
+            q *= nums[i];
+            res[i - 1] *= q;
+        }
+        return res;
+    }
+}
