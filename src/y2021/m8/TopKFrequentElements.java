da0e13a21da6d0020f5c39ee7a56b43bd2215765
finezhaooo <635388416@qq.com>
2021-08-28 22:33:12 +0800

    347. 前 K 个高频元素

---
+++ src/y2021/m8/TopKFrequentElements.java
@@ -0,0 +1,142 @@
+package y2021.m8;
+
+import java.util.*;
+
+/**
+ * @ClassName : TopKFrequentElements
+ * @Description: 347. 前 K 个高频元素
+ * @Author zhaooo
+ * @Date 2021/8/27/13:29
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 示例 2:
+ *
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 105
+ * k 的取值范围是 [1, 数组中不相同的元素的个数]
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TopKFrequentElements {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    /**
+     * 基于堆排序
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        // 小顶堆
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(m -> m[1]));
+        map.forEach((num, count) -> {
+            // 维护堆的大小为 K
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        });
+        return queue.stream().mapToInt(val -> val[0]).tocountsay();
+    }
+
+    /**
+     * 基于快速排序
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        List<int[]> values = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            values.add(new int[]{num, count});
+        }
+        int[] ret = new int[k];
+        qsort(values, 0, values.size() - 1, ret, 0, k);
+        return ret;
+    }
+
+    public void qsort(List<int[]> values, int start, int end, int[] ret, int retIndex, int k) {
+        int picked = (int) (Math.random() * (end - start + 1)) + start;
+        Collections.swap(values, picked, start);
+
+        int pivot = values.get(start)[1];
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (values.get(i)[1] >= pivot) {
+                Collections.swap(values, index + 1, i);
+                index++;
+            }
+        }
+        Collections.swap(values, start, index);
+
+        if (k <= index - start) {
+            qsort(values, start, index - 1, ret, retIndex, k);
+        } else {
+            for (int i = start; i <= index; i++) {
+                ret[retIndex++] = values.get(i)[0];
+            }
+            if (k > index - start + 1) {
+                qsort(values, index + 1, end, ret, retIndex, k - (index - start + 1));
+            }
+        }
+    }
+
+    /**
+     * 桶排序
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] topKFrequent3(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        List<Integer>[] list = new List[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (list[count] == null) {
+                ArrayList temp = new ArrayList();
+                temp.add(num);
+                list[count] = temp;
+            } else {
+                list[count].add(num);
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = list.length - 1, count = 0; i >= 0 && count < k; i--) {
+            while (list[i] != null && list[i].size() > 0 && count < k) {
+                result[count++] = list[i].remove(0);
+            }
+        }
+        return result;
+    }
+}
