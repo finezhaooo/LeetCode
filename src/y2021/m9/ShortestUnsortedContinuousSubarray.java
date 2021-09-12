4285d2fb4658c06e4367774ab5c83652eb004db6
finezhaooo <635388416@qq.com>
2021-09-12 13:33:32 +0800

    581. 最短无序连续子数组

---
+++ src/y2021/m9/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,131 @@
+package y2021.m9;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * @ClassName : ShortestUnsortedContinuousSubarray
+ * @Description: 581. 最短无序连续子数组
+ * @Author zhaooo
+ * @Date 2021/9/11/17:13
+ * 给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+ *
+ * 请你找出符合题意的 最短 子数组，并输出它的长度。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,6,4,8,10,9,15]
+ * 输出：5
+ * 解释：你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,4]
+ * 输出：0
+ * 示例 3：
+ *
+ * 输入：nums = [1]
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ *
+ *
+ * 进阶：你可以设计一个时间复杂度为 O(n) 的解决方案吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ShortestUnsortedContinuousSubarray {
+
+    /**
+     * 排序
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        if (isSorted(nums)) {
+            return 0;
+        }
+        int[] numsSorted = new int[nums.length];
+        System.arraycopy(nums, 0, numsSorted, 0, nums.length);
+        Arrays.sort(numsSorted);
+        int left = 0;
+        while (nums[left] == numsSorted[left]) {
+            left++;
+        }
+        int right = nums.length - 1;
+        while (nums[right] == numsSorted[right]) {
+            right--;
+        }
+        return right - left + 1;
+    }
+
+    public boolean isSorted(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 单调栈:分别遍历出左右下标
+     * 单调栈可以解决往前或者往后的最大值最小值
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarray2(int[] nums) {
+        Deque<Integer> stack = new ArrayDeque<>(nums.length);
+        int left = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                left = Math.min(left, stack.pop());
+            }
+            stack.push(i);
+        }
+        if (stack.size() == nums.length) {
+            return 0;
+        }
+        stack.clear();
+        int right = -1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                right = Math.max(right, stack.pop());
+            }
+            stack.push(i);
+        }
+        return right - left + 1;
+    }
+
+    /**
+     * 一次遍历
+     * https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/solution/zui-duan-wu-xu-lian-xu-zi-shu-zu-by-leet-yhlf/
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarray3(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
+    }
+}
